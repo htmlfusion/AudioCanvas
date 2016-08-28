e0d956288201203718a4ef2385af547ef02ae600
AnthonyCorrado <amcstudios@yahoo.com>
2016-08-27 17:56:06 -0700

    adds base EventManager with test

---
+++ Assets/Utilities/Tests/Scripts/EventTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class EventTest : MonoBehaviour {
+
+    private UnityAction testListener;
+
+    void Awake()
+    {
+        testListener = new UnityAction(TestFunction);
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("test", testListener);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("test", testListener);
+    }
+
+    void TestFunction()
+    {
+        Debug.Log("Test function was called");
+    }
+}
